7eefe210d0ab5928f34eef00039a9afaaa52ed8d
BradHuang <adghbc01@tamu.edu>
2018-05-06 20:28:44 +0000

    data types.

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,41 @@
-console.log("Hello, JS!");
+// console.log("Hello, JS!");
+
+/*
+multi-line comment
+*/
+
+/*
+var name = "Brad";
+console.log(name);
+
+var lastName = "Hu";
+console.log(lastName);
+
+var age = 24;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// var name = "Brad";
+// var age = 24;
+// // console.log(name + age); // Brad24
+// // console.log(age + age); // 48
+
+// var job, isMarried;
+// // console.log(job); // undefined, no value yet.
+
+// job = "SDE";
+// isMarried = false;
+// console.log(name + " is " + age + " year-old " + job + ", is he married? " + isMarried + ".");
+
+// age = "twenty-four"; // Dynamic Typing!
+// job = "Singer";
+// console.log(name + " is " + age + " year-old " + job + ", is he married? " + isMarried + ".");
+
+// var firstName = prompt("What is your firstName?");
+// console.log(firstName);
+
+// alert("Session finished!");
+
