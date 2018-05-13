b758d43f4225d2c0ec89690dddfcd41914c3e5e2
BradDHuang <adghbc01@tamu.edu>
2018-05-13 18:21:50 +0000

    6 steps for an event

--- 6-Budget-App/app.js
+++ 6-Budget-App/app.js
@@ -1,16 +1,19 @@
 var budgetController = (function() {
     
-    var x = 23;
-    var add = function(a) {
-        return x + a;
-    }
+    // var x = 23;
+    // var add = function(a) {
+    //     return x + a;
+    // }
     
-    return {
-        publicTest: function(b) {
-            // console.log(add(b));
-            return add(b);
-        }
-    }; // return an obj.
+    // return {
+    //     publicTest: function(b) {
+    //         // console.log(add(b));
+    //         return add(b);
+    //     }
+    // }; // return an obj.
+    
+    
+    
 })(); // IIFE
 // budgetController.x // undefined
 // budgetController.add(5) // Uncaught TypeError: budgetController.add is not a function
@@ -22,9 +25,9 @@ var UIController = (function() {
     
     
 })();
-
+// global app controller
 var controller = (function(budgetCtrl, UICtrl) {
-    
+    /*
     var z = budgetCtrl.publicTest(5);
     
     return {
@@ -32,6 +35,23 @@ var controller = (function(budgetCtrl, UICtrl) {
             console.log(z);
         }
     };
+    */
+    
+    // step 1: add event handler.
+    document.querySelector(".add__btn").addEventListener("click", function() {
+        // console.log("this is a test: btn was clicked.");
+        
+        // step 2: get the field input data.
+        
+        // step 3: add the item to the budget controller.
+        
+        // step 4: add the item to the UI.
+        
+        // step 5: calculate budget.
+        
+        // step 6: update UI.
+        
+    });
     
 })(budgetController, UIController);
 // controller.publicTest2() // 28
