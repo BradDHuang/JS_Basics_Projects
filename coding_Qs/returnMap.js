ee211d99e16af79570fcab6cf05f648cce08f08d
BradDHuang <adghbc01@tamu.edu>
2018-09-25 04:13:11 +0000

    the Map obj.

---
+++ coding_Qs/returnMap.js
@@ -0,0 +1,30 @@
+
+// Given an array and a function, 
+// write a function that returns a map 
+// where the keys are the items of the array 
+// and the values are the returning output of executing the function by passing item as parameter. 
+
+let arr = [1,2,3,4,5];
+
+function double(param) {
+    return param * 2;
+}
+
+let map = new Map();
+
+function returnMap(array) {
+    for (let i = 0; i < arr.length; i++) {
+        map.set(array[i], double(array[i]));
+    }
+    return map;
+}
+
+for (let [key, val] of returnMap(arr)) {
+    console.log(key + " double gets: " + val);
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+
+
