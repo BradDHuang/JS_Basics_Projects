4f12db090d6a306ab216971320bcd515e2484468
BradDHuang <adghbc01@tamu.edu>
2018-10-19 19:11:59 +0000

    review: cut sticks, alt.

--- coding_Qs/cutSticks.js
+++ coding_Qs/cutSticks.js
@@ -31,6 +31,36 @@ const arr2 = [5,4,4,2,2,8]; // [6,4,2,1]
 console.log(cutSticks(arr2));
 
 
+// method2
+
+// cut the sticks
+
+let lens = [1, 1, 2, 3];
+// 1 1 2 3, min = 1, cuts = 4;
+// - - 1 2, min = 1, cuts = 2;
+// - - - 1, min = 1, cuts = 1;
+// res = [4, 2, 1]
+
+let lens2 = [5,4,4,2,2,8];
+
+function cutSticks(lens) {
+  let res = [];
+  while (lens.length > 0) {
+    res.push(lens.length);
+    lens.sort((a, b) => a - b);
+    let min = lens[0];
+    for (let i = 0; i < lens.length; i++) {
+      lens[i] -= min;
+    }
+    let lastZeroIdx = lens.lastIndexOf(0);
+    lens.splice(0, lastZeroIdx + 1);
+  }
+  return res;
+}
+
+console.log(cutSticks(lens2));
+
+
 
 
 
