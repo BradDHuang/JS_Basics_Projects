0269d9e0f2f51e061bbf126ecb0d042ad61a3cd2
BradDHuang <adghbc01@tamu.edu>
2018-11-09 20:17:00 +0000

    Review of ES6: Arrow func. =>

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -63,7 +63,7 @@ function dl6(passed) {
 dl6(true);
 // John was born in 1990
 */
-
+/*
 let i = 10;
 for (let i = 0; i < 5; i++) {
     console.log(i);
@@ -79,11 +79,130 @@ for (var j = 0; j < 5; j++) {
 }
 console.log(j);
 // 5
-
+*/
 
 // (2) Template literals (Template strings) with backticks
 // e.g.
 // `string text ${expression} string text`
+
+
+// (3) arrow functions
+
+// Using map() with index:
+// const years = [1990, 1992, 1995];
+
+// const curYear = new Date().getFullYear();
+// let ages = years.map((e, index) => `index of ${index} has age of ${curYear - e}`);
+// console.log(ages);
+
+// ES5 
+/*
+var obj = {
+    color: "green",
+    clicked: function() {
+        console.log(this); // {color: "green", clicked: ƒ}
+        document.querySelector(".green")
+        .addEventListener("click", function() {
+            // console.log(this);
+            alert(`You clicked a box of color ${this.color}`);
+        });
+    }
+};
+obj.clicked();
+// You clicked a box of color undefined
+*/
+/*
+var obj2 = {
+    color: "green",
+    clicked: function() {
+        // console.log(this); // {color: "green", clicked: ƒ}
+        var self = this;
+        document.querySelector(".green")
+        .addEventListener("click", function() {
+            // console.log(this);
+            alert(`You clicked a box of color ${self.color}`);
+        });
+    }
+};
+obj2.clicked();
+// You clicked a box of color green
+*/
+
+// ES6
+/*
+const obj3 = {
+    color: "green",
+    clicked: function() {
+        // console.log(this); // obj3 {}
+        document.querySelector(".green")
+        .addEventListener("click", () => {
+            // console.log(this); // obj3{}
+            alert(`You clicked a box of color ${this.color}`);
+        });
+    }
+};
+obj3.clicked();
+// You clicked a box of color green
+*/
+
+// In arrow functions, this retains the value of the enclosing lexical context's this.
+
+// Arrow functions follow the normal variable lookup rules. 
+// So while searching for this which is not present in current scope 
+// they end up finding this from its enclosing scope. 
+
+
+
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+/*
+Person.prototype.myFriends = function(friends) {
+    var list = friends.map(function(friend) {
+        console.log(this); // Window {}
+        return this.name + " is friend of " + friend;
+    });
+    console.log(list); 
+};
+
+var friends = ["Bob", "Chris"];
+new Person("Ben").myFriends(friends);
+// [" is friend of Bob", " is friend of Chris"]
+*/
+
+/*
+// Use bind()
+Person.prototype.myFriends = function(friends) {
+    var list = friends.map(function(friend) {
+        console.log(this); // Person {name: "Ben"}
+        return this.name + " is friend of " + friend;
+    }.bind(this));
+    console.log(list); 
+};
+
+var friends = ["Bob", "Chris"];
+new Person("Ben").myFriends(friends);
+// ["Ben is friend of Bob", "Ben is friend of Chris"]
+*/
+
+
+// ES6
+
+Person.prototype.myFriends = function(friends) {
+    const list = friends.map((friend) => {
+        console.log(this); // Person {name: "Ben"}
+        return this.name + " is friend of " + friend;
+    });
+    console.log(list); 
+};
+
+const friends = ["Bob", "Chris"];
+new Person("Ben").myFriends(friends);
+// ["Ben is friend of Bob", "Ben is friend of Chris"]
+
+
 
 
 
