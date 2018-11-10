c5bc00673756ec17385fd79692545729105e8106
BradDHuang <adghbc01@tamu.edu>
2018-11-10 06:20:26 +0000

    Review of ES6: Spread Operator

--- 7-ES6/script2.js
+++ 7-ES6/script2.js
@@ -4,7 +4,7 @@
 
 
 // Destructuring:
-
+/*
 const John = ["John", 24];
 const [name, age] = John;
 console.log(name, age);
@@ -31,11 +31,11 @@ function calcRetireAge(yearOfBirth) {
 }
 const [curAge, yearsToRetire] = calcRetireAge(1995);
 console.log("curAge: " + curAge, "yearsToRetire: " + yearsToRetire);
-
+*/
 
 // Array
 
-const boxes = document.querySelectorAll(".box");
+// const boxes = document.querySelectorAll(".box");
 // returns a static NodeList object.
 
 // ES5
@@ -52,7 +52,7 @@ for (var i = 0; i < boxesArr.length; i++) {
     boxesArr[i].textContent = "I changed to blue now.";
 }
 */
-
+/*
 // ES6
 const boxesArr = Array.from(boxes);
 boxesArr.forEach(e => e.style.backgroundColor = "orangered");
@@ -73,6 +73,31 @@ console.log(ages[fullAgeIdx] + " >= 18.");
 
 // Or use:
 console.log(ages.find(e => e >= 18) + " >= 18.");
+*/
+
+
+// Spread Operator
+/*
+// ES5
+function sum(a, b, c, d) {
+    return a + b + c + d;
+}
+
+var nums = [10, 20, 30, 42];
+
+console.log(sum.apply(null, nums)); // 102
+
+// ES6
+console.log(sum(...nums)); // 102
+
+// use case e.g.
+
+const h = document.querySelector("h1");
+const boxes = document.querySelectorAll(".box");
+const all = [h, ...boxes];
+Array.from(all).forEach(e => e.style.color = "purple");
+*/
+
 
 
 
