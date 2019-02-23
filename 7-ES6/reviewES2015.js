52d9ba933664d376db87022cf7ba1b66d7cd07e3
BradDHuang <adghbc01@tamu.edu>
2019-02-23 23:02:20 +0000

    ES6 Recap

---
+++ 7-ES6/reviewES2015.js
@@ -0,0 +1,32 @@
+
+// ES2015 review
+
+// 1. const, can mutate if its an object, 
+    // but not declare again.
+    
+// 2. let, 
+
+// A TypeError 
+// is thrown when an operand or argument passed to a function is incompatible with the type expected by that operator or function.
+
+// One-line arrow func.
+// e.g. [1,22,333].map(val => val * 2).filter(n => n % 3 === 0);
+// Arrow func.s do Not get their own 'this' keyword
+
+// Inside of an Arrow func., the keyword 'this'
+// has its Original meaning from the enclosing context.
+
+// Arrow func. should Never be used as methods in objects
+// since we will get the incorrect value of the keyword 'this'.
+
+// Default Param.s
+
+// Obj. Destructuring
+
+// Obj. fields as param.s
+
+// Array Destructuring
+
+
+
+
